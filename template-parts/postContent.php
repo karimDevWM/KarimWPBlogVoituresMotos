50cadea5349e867c6d303990ae3111a7c3803983
karimDevWM <ka.trabelsi9@gmail.com>
2022-03-30 14:15:37 +0200

    modifs

--- template-parts/postContent.php
+++ template-parts/postContent.php
@@ -4,20 +4,20 @@
         <div class="card-body">
             <p class="card-text">Date de création : <?= get_the_date(); ?></p>
             <p class="card-text">Catégorie : <?= the_category(); ?></p>
-            <p class="card-text">Auteur : <?= get_the_author(); ?></p>
+            <p class="card-text">Auteur : <?php echo the_author_meta(); ?></p>
             <p class="card-text">Content : <?= get_the_content(); ?></p>
-            <p><strong>date de sortie :</strong>
-                <?php echo get_field('date_sortie'); ?>
+            <p><strong>age :</strong>
+                <?php echo get_post_meta( get_the_ID(), 'age', true ); ?>
             </p>
             <p>
-                <strong>puissance :</strong>
-                <?php echo get_field('puissance_en_chevaux_ou_cm3'); ?>
+                <strong>race :</strong>
+                <?php echo get_post_meta(get_the_ID(), 'race', true ); ?>
             </p>
         </div>
     </div>
         <div class="pagination mt-5">
         <div class="pagination_prev">
-        <?php previous_post_link( 'Article Précédent<br>%link' ); ?>
+                <?php previous_post_link( 'Article Précédent<br>%link' ); ?>
             </div>
             <div class="pagination_next">
                 <?php next_post_link('Article Suivant<br>%link' ); ?> 
