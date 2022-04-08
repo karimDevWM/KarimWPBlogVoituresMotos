940c4f5c6f1806f4a680f1eb9f5f87450d0e28b5
karimDevWM <ka.trabelsi9@gmail.com>
2022-04-09 00:02:14 +0200

    ajout acf dans le single de portfolio et ajout grid dans le css

--- template-parts/garage/garage-single.php
+++ template-parts/garage/garage-single.php
@@ -1,13 +1,28 @@
-<div class="single-garage">
+<div class="car-spec-head">
+    <h3>Presentation <?= the_field('modele')."-".get_the_title(); ?></h3>
+    <p><?php the_post_thumbnail('large'); ?></p>
+</div>
+<div class="car-spec-sheet">
+    <div class="fields-labels">
+        <p>modele : </p>
+        <p>annee : </p>
+    </div>
+    <div class="fields-content">
+        <p><?= the_field('modele'); ?></p>
+        <p><?= the_field('annee'); ?></p>
+    </div>
+
+</div>
+
+<!--============================================================================-->
                     <div class="card">
-                        <div><?php echo get_the_post_thumbnail('medium'); ?></div>
                         <div class="card-body">
                             <h5 class="card-title"><?= get_the_title(); ?></h5>
-                            <p class="card-text"><?= get_the_content(); ?></p>
-                            <p class="card-text">Catégorie : <?= the_category(); ?></p>
+                            <p class="card-text">Catégorie : <?php get_the_category(); ?></p>
                             <p class="card-text">Auteur : <?= get_the_author(); ?></p>
-                            <p class="card-text">Date de création : <?= get_the_date(); ?></p>
+                            <p class="card-text">Date de publication : <?= get_the_date(); ?></p>
+                            
+                            
                         </div>
                     </div>
             <hr>
-    </div>
