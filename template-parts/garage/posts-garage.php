ff2355b0744e7da6c799771fcbc0a0d491a6874e
karimDevWM <ka.trabelsi9@gmail.com>
2022-04-15 10:39:58 +0200

    modif function

--- template-parts/garage/posts-garage.php
+++ template-parts/garage/posts-garage.php
@@ -25,7 +25,7 @@
                     <div class="card-body">
                         <h5 class="card-title"><?= get_the_title(); ?></h5>
                         <p class="card-text"><?= get_the_excerpt(); ?></p>
-                        <p class="card-text">Catégorie : <?= the_category(); ?></p>
+                        <p class="card-text">Catégorie : <?php get_the_category(); ?></p>
                         <p class="card-text">Auteur : <?= get_the_author(); ?></p>
                         <p class="card-text">Date de création : <?= get_the_date(''); ?></p>
                         <a href="<?= the_permalink(); ?>" class="btn btn-primary">Lire l'article</a>
